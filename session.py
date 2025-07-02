480ccaf5038c7e03d59529ab8870f7623c72eaf9
AndreiStan02 <andreistandima2323@gmail.com>
2025-07-02 02:33:14 +0200

    userProfile calculations and user create/log added

--- session.py
+++ session.py
@@ -1,11 +1,43 @@
+import json
+
 from userProfile import UserProfile, ActivityFactor
 
 class Session:
     def __init__(self):
-        self.profiles = {}
+        self.profiles = []
+        self.currentProfile = None
+        self.get_profiles()
 
-    def createProfile(self, name, weight, age, height, gender, activity, fat_per):
-        new_profile = UserProfile(name, weight, age, height, gender, activity, fat_per)
+    def createProfile(self, name, weight, age, height, gender, activity, fat_per, goal_per):
+        data = {
+            "name": name,
+            "weight":weight,
+            "age":age,
+            "height":height,
+            "gender":gender,
+            "activity":activity,
+            "fat_per":fat_per,
+            "goal_per":goal_per,
+            "all_measures":[(weight,fat_per)],
+            "week_measures":[(weight,fat_per)],
+        }
+
+        new_profile = UserProfile(name, weight, age, height, gender, activity, fat_per, goal_per)
         if self.profiles[new_profile.name]:
             raise Exception("User already exists")
-        self.profiles.update({new_profile.name: new_profile})
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
+        self.get_profiles()
+
+    def logIn(self, name):
+        if self.profiles[name]:
+            self.currentProfile = self.profiles[name]
+        else:
+            raise Exception("No user with this name exists")
+
+    def get_profiles(self):
+        with open("users.json", "r") as file:
+            data = json.load(file)
+        for profile in data:
+            aux_profile = UserProfile(data.name, data.weight, data.age, data.height, data.gender, data.activity, data.fat_per, data.goal_per, data.all_measures, data.week_measures)
+            self.profiles.append((data.name, aux_profile))
--- userProfile.py
+++ userProfile.py
@@ -6,7 +6,7 @@ class ActivityFactor(Enum):
     VERY = 3
 
 class UserProfile:
-    def __init__(self, name, weight, age, height, gender, activity, fat_per):
+    def __init__(self, name, weight, age, height, gender, activity, fat_per, goal_per, all_measures = [], week_measures = []):
         self.name = name
         self.weight = weight
         self.age = age
@@ -14,18 +14,24 @@ class UserProfile:
         self.gender = gender
         self.activity = activity
         self.fat_per = fat_per
+        self.goal_per = goal_per
+        self.goal_time = get_goal_time(self) * 7
         self.tdee = get_tdee(self)
-        self.all_measures = []
-        self.week_measures = []
+        self.cal_intake = get_cal(self)
+        if all_measures == []:
+            self.all_measures = [(weight, fat_per)]
+        else:
+            self.all_measures = all_measures
+        if week_measures == []:
+            self.week_measures = [(weight, fat_per)]
+        else:
+            self.week_measures = week_measures
 
     def update_weight(self, new_weight):
         self.weight = new_weight
-        self.tdee = get_tdee(self)
-        return self.tdee
 
     def update_fat_per(self, new_fat):
         self.fat_per = new_fat
-
 
 def get_tdee(profile):
     activity_factor = 0
@@ -43,6 +49,47 @@ def get_tdee(profile):
         return (10 * profile.weight + 6.25 * profile.height - 5 * profile.age + 5) * activity_factor
     return (10 * profile.weight + 6.25 * profile.height - 5 * profile.age - 161) * activity_factor
 
+def get_cal(profile):
+    target_weight = (profile.weight - (profile.weight*profile.fat_per)) / (1 - profile.goal_per)
+    total_cal = 7700 * (profile.weight - target_weight)
+    cal_per_day = total_cal/profile.goal_time
+    return profile.tdee - cal_per_day
 
-#yo = UserProfile("andrei", 80, 23, 178, "male", ActivityFactor.MODERATE, 22)
-#print(yo.tdee)
+def get_goal_time(profile):
+    target_weight = (profile.weight - (profile.weight*profile.fat_per)) / (1 - profile.goal_per)
+    return (profile.weight - target_weight)/0.5
+
+def add_measure(profile, weight, fat_per):
+    profile.update_weight(weight)
+    profile.update_fat_per(fat_per)
+
+    profile.all_measures.append((weight, fat_per))
+    profile.week_measures.append((weight, fat_per))
+
+    if len(profile.week_measures) >= 7:
+        total_weight = 0
+        avr_weight = 0
+        espected_weight = profile.week_measures[0][0] - 0.5
+        for measure in profile.week_measures:
+            total_weight += measure[0]
+        avr_weight = total_weight/7
+        if abs(espected_weight - avr_weight) >= 0.5:
+            if espected_weight > avr_weight:
+                profile.tdee += 100
+            else:
+                profile.tdee -= 100
+        profile.weeñ_measures = []
+    
+    #just say how many days left there are and how the weight has been the other days
+    #MAYBE THIS BETTER IN SESSION
+
+
+yo = UserProfile("andrei", 80, 23, 178, "male", ActivityFactor.MODERATE, 0.22, 0.18)
+print(yo.tdee)
+add_measure(yo, 76, 22)
+add_measure(yo, 76, 22)
+add_measure(yo, 74, 22)
+add_measure(yo, 73, 22)
+add_measure(yo, 75, 22)
+add_measure(yo, 72, 22)
+print(yo.tdee)
